d26561cf93a421deb8614f90bead0444b1f2eeec
Mark Celoza <markangeloceloza380@gmail.com>
2024-05-27 20:26:21 +0800

    fix: remove auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,64 +1,61 @@
-import bcrypt from "bcrypt";
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { authConfig } from "./authconfig";
-import { Users } from "./model";
-import { connectToDb } from "./utils";
+// import bcrypt from "bcrypt";
+// import { Users } from "./model";
+// import { connectToDb } from "./utils";
 
-const login = async (credentials) => {
-  try {
-    connectToDb();
-    const user = await Users.findOne({ username: credentials.username });
+// export const login = async (credentials) => {
+//   try {
+//     connectToDb();
+//     const user = await Users.findOne({ username: credentials.username });
 
-    if (!user) {
-      throw new Error("Wrong credentials!");
-    }
+//     if (!user) {
+//       throw new Error("Wrong credentials!");
+//     }
 
-    const isPasswordCorrect = await bcrypt.compare(
-      credentials.password,
-      user.password
-    );
+//     const isPasswordCorrect = await bcrypt.compare(
+//       credentials.password,
+//       user.password
+//     );
 
-    if (!isPasswordCorrect) {
-      throw new Error("Wrong credentials!");
-    }
+//     if (!isPasswordCorrect) {
+//       throw new Error("Wrong credentials!");
+//     }
 
-    return user;
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to login!");
-  }
-};
+//     return user;
+//   } catch (err) {
+//     console.log(err);
+//     throw new Error("Failed to login!");
+//   }
+// };
 
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials) {
-        try {
-          const user = await login(credentials);
-          return user;
-        } catch (err) {
-          return null;
-        }
-      },
-    }),
-  ],
-  // ADD ADDITIONAL INFORMATION TO SESSION
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.username = user.username;
-        token.image = user.image;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user.username = token.username;
-        session.user.image = token.image;
-      }
-      return session;
-    },
-  },
-});
+// // export const { auth, signIn, signOut } = NextAuth({
+// //   ...authConfig,
+// //   providers: [
+// //     CredentialsProvider({
+// //       async authorize(credentials) {
+// //         try {
+// //           const user = await login(credentials);
+// //           return user;
+// //         } catch (err) {
+// //           return null;
+// //         }
+// //       },
+// //     }),
+// //   ],
+// //   // ADD ADDITIONAL INFORMATION TO SESSION
+// //   callbacks: {
+// //     async jwt({ token, user }) {
+// //       if (user) {
+// //         token.username = user.username;
+// //         token.image = user.image;
+// //       }
+// //       return token;
+// //     },
+// //     async session({ session, token }) {
+// //       if (token) {
+// //         session.user.username = token.username;
+// //         session.user.image = token.image;
+// //       }
+// //       return session;
+// //     },
+// //   },
+// // });
